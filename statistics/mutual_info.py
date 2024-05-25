ee29833a406e9f8a4fa0a8459d3dc8167ad02f77
Tobias Ritter <tobias.ritter98@gmail.com>
2024-05-25 11:53:19 +0200

    add random_state param

--- statistics/mutual_info.py
+++ statistics/mutual_info.py
@@ -2,15 +2,29 @@ import pandas as pd
 from sklearn.feature_selection import mutual_info_regression
 
 
-def mutual_info(x: pd.DataFrame, y: pd.Series, n_neighbors: int = 3) -> pd.Series:
+def mutual_info(
+    x: pd.DataFrame,
+    y: pd.Series,
+    n_neighbors: int = 3,
+    random_state: int | None = None,
+) -> pd.Series:
     mi = mutual_info_regression(
-        x, y, n_neighbors=n_neighbors, discrete_features=False, random_state=42
+        x,
+        y,
+        n_neighbors=n_neighbors,
+        discrete_features=False,
+        random_state=random_state,
     )
     return pd.Series(mi, index=x.columns, name=y.name)
 
 
 def mutual_info_df(
-    x: pd.DataFrame, y: pd.DataFrame, n_neighbors: int = 3
+    x: pd.DataFrame,
+    y: pd.DataFrame,
+    n_neighbors: int = 3,
+    random_state: int | None = None,
 ) -> pd.DataFrame:
-    mi_series = [mutual_info(x, y[target], n_neighbors) for target in y.columns]
+    mi_series = [
+        mutual_info(x, y[target], n_neighbors, random_state) for target in y.columns
+    ]
     return pd.concat(mi_series, axis=1)
