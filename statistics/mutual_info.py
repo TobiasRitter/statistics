1f45df0ae9f328538249f0a9031524424f1edfe7
Tobias Ritter <tobias.ritter98@gmail.com>
2024-05-25 11:50:55 +0200

    add mutual_info_df

--- statistics/mutual_info.py
+++ statistics/mutual_info.py
@@ -7,3 +7,10 @@ def mutual_info(x: pd.DataFrame, y: pd.Series, n_neighbors: int = 3) -> pd.Serie
         x, y, n_neighbors=n_neighbors, discrete_features=False, random_state=42
     )
     return pd.Series(mi, index=x.columns, name=y.name)
+
+
+def mutual_info_df(
+    x: pd.DataFrame, y: pd.DataFrame, n_neighbors: int = 3
+) -> pd.DataFrame:
+    mi_series = [mutual_info(x, y[target], n_neighbors) for target in y.columns]
+    return pd.concat(mi_series, axis=1)
--- tests/mutual_info_test.py
+++ tests/mutual_info_test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from statistics.mutual_info import mutual_info
+from statistics.mutual_info import mutual_info, mutual_info_df
 
 
 def test_mutual_info() -> None:
@@ -11,6 +11,27 @@ def test_mutual_info() -> None:
         }
     )
     y = pd.Series([21, 22, 23, 24, 25, 26, 27, 28, 29], name="target")
-    mi = mutual_info(x, y)
-    assert len(mi) == 3
-    assert mi.name == "target"
+
+    result = mutual_info(x, y)
+    assert len(result) == 3
+    assert result.name == "target"
+
+
+def test_mutual_info_df() -> None:
+    x = pd.DataFrame(
+        {
+            "a": [21, 22, 23, 24, 25, 26, 27, 28, 29],
+            "b": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            "c": [-10, 5, 30, -12, -50, 100, 20, 13, -90],
+        }
+    )
+    y = pd.DataFrame(
+        {
+            "x": [21, 22, 23, 24, 25, 26, 27, 28, 29],
+            "y": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            "z": [-10, 5, 30, -12, -50, 100, 20, 13, -90],
+        }
+    )
+
+    result = mutual_info_df(x, y)
+    assert result.shape == (3, 3)
