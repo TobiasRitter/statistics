e1620cc840f91d3a8ee9aaf6e47c40c3904ff810
Tobias Ritter <40394437+TobiasRitter@users.noreply.github.com>
2024-07-07 10:46:55 +0200

    Add model eval class (#3)
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Tobias Ritter <tobias.ritter98@gmail.com>

---
+++ statistics/model_eval.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+import pandas as pd
+from sklearn.metrics import (
+    mean_squared_error,
+    r2_score,
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+)
+
+
+@dataclass
+class Predictions:
+    y_pred_train: pd.Series
+    y_pred_test: pd.Series
+    y_true_train: pd.Series
+    y_true_test: pd.Series
+
+
+@dataclass
+class ModelEval:
+    r2: float
+    mse: float
+    mae: float
+    mape: float
+
+    @classmethod
+    def metrics(cls) -> list[str]:
+        return list(cls.__annotations__.keys())
+
+    @classmethod
+    def from_predictions(cls, predictions: Predictions) -> "ModelEval":
+        return cls(
+            r2=r2_score(predictions.y_true_test, predictions.y_pred_test),
+            mse=mean_squared_error(predictions.y_true_test, predictions.y_pred_test),
+            mae=mean_absolute_error(predictions.y_true_test, predictions.y_pred_test),
+            mape=mean_absolute_percentage_error(
+                predictions.y_true_test, predictions.y_pred_test
+            ),
+        )
