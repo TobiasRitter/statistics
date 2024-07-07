7aef1cdd4291ddd80fa74abe011bb806d45bd10b
Tobias Ritter <tobias.ritter98@gmail.com>
2024-07-07 18:03:26 +0200

    update

--- statistics/xai.py
+++ statistics/xai.py
@@ -6,34 +6,44 @@ from omnixai.data.tabular import Tabular
 from omnixai.explainers.tabular import TabularExplainer
 from omnixai.explanations.base import ExplanationBase
 from plotly.graph_objects import Figure
+from sklearn.model_selection import train_test_split
 
 
 def plot_explanation(explanation: ExplanationBase, path: Path) -> None:
     fig: Figure = explanation.plotly_plot().component
     fig.write_html(path)
-    pass
 
 
-def explain(model, x: pd.DataFrame, y: pd.Series) -> None:
+def explain(
+    model,
+    x_train: pd.DataFrame,
+    y_train: pd.Series,
+    x_val: pd.DataFrame,
+    y_val: pd.DataFrame,
+) -> None:
     explainers = TabularExplainer(
         explainers=["lime", "shap", "sensitivity", "pdp", "ale"],
         mode="regression",
-        data=Tabular(pd.concat([x, y], axis=1), target_column=y.name),
+        data=Tabular(pd.concat([x_train, y_train], axis=1), target_column=y_train.name),
         model=model,
         preprocess=lambda tabular: tabular.data.values,
     )
-    explanations = explainers.explain_global()
+    local_explains = explainers.explain(x_val)
+    global_explains = explainers.explain_global()
 
-    plot_explanation(explanations["sensitivity"], Path("sensitivity.html"))
-    plot_explanation(explanations["pdp"], Path("pdp.html"))
-    plot_explanation(explanations["ale"], Path("ale.html"))
+    plot_explanation(local_explains["lime"], Path("lime.html"))
+    plot_explanation(local_explains["shap"], Path("shap.html"))
+    plot_explanation(global_explains["sensitivity"], Path("sensitivity.html"))
+    plot_explanation(global_explains["pdp"], Path("pdp.html"))
+    plot_explanation(global_explains["ale"], Path("ale.html"))
 
 
 def main() -> None:
     x, y = load_diabetes(return_X_y=True, as_frame=True)
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
     rf = RandomForestRegressor()
     rf.fit(x, y.values.ravel())
-    explain(rf, x, y)
+    explain(rf, x_train, y_train, x_val, y_val)
 
 
 if __name__ == "__main__":
