e795353bcdf1672e4e9be045635d4b16c4a7186e
Tobias Ritter <tobias.ritter98@gmail.com>
2024-07-07 11:58:39 +0200

    extract function

--- statistics/xai.py
+++ statistics/xai.py
@@ -6,6 +6,21 @@ from omnixai.data.tabular import Tabular
 from omnixai.explainers.tabular import TabularExplainer
 
 
+def explain(model, x: pd.DataFrame, y: pd.DataFrame) -> None:
+    explainers = TabularExplainer(
+        explainers=["lime", "shap", "sensitivity", "pdp", "ale"],
+        mode="regression",
+        data=Tabular(pd.concat([x, y], axis=1), target_column="target"),
+        model=model,
+        preprocess=lambda tabular: tabular.data.values,
+    )
+    explanations = explainers.explain_global()
+
+    explanations["sensitivity"].ipython_plot()
+    explanations["pdp"].ipython_plot()
+    explanations["ale"].ipython_plot()
+
+
 def main() -> None:
     housing = fetch_california_housing()
     df = pd.DataFrame(
@@ -16,19 +31,7 @@ def main() -> None:
 
     rf = RandomForestRegressor()
     rf.fit(x, y.values.ravel())
-
-    explainers = TabularExplainer(
-        explainers=["lime", "shap", "sensitivity", "pdp", "ale"],
-        mode="regression",
-        data=Tabular(pd.concat([x, y], axis=1), target_column="target"),
-        model=rf,
-        preprocess=lambda tabular: tabular.data.values,
-    )
-    explanations = explainers.explain_global()
-
-    explanations["sensitivity"].ipython_plot()
-    explanations["pdp"].ipython_plot()
-    explanations["ale"].ipython_plot()
+    explain(rf, x, y)
 
 
 if __name__ == "__main__":
