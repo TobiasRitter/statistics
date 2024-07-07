159e84ce009c2bdfbd04a811a177fb4f547bde8a
Tobias Ritter <tobias.ritter98@gmail.com>
2024-07-07 12:04:08 +0200

    update

--- statistics/xai.py
+++ statistics/xai.py
@@ -6,11 +6,11 @@ from omnixai.data.tabular import Tabular
 from omnixai.explainers.tabular import TabularExplainer
 
 
-def explain(model, x: pd.DataFrame, y: pd.DataFrame) -> None:
+def explain(model, x: pd.DataFrame, y: pd.Series) -> None:
     explainers = TabularExplainer(
         explainers=["lime", "shap", "sensitivity", "pdp", "ale"],
         mode="regression",
-        data=Tabular(pd.concat([x, y], axis=1), target_column="target"),
+        data=Tabular(pd.concat([x, y], axis=1), target_column=y.name),
         model=model,
         preprocess=lambda tabular: tabular.data.values,
     )
@@ -27,7 +27,7 @@ def main() -> None:
         np.concatenate([housing.data, housing.target.reshape((-1, 1))], axis=1),
         columns=list(housing.feature_names) + ["target"],
     )
-    x, y = df[list(housing.feature_names)], df[["target"]]
+    x, y = df[list(housing.feature_names)], df["target"]
 
     rf = RandomForestRegressor()
     rf.fit(x, y.values.ravel())
